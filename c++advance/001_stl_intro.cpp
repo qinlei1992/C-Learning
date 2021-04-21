e231c427ba199eec93e5ca6a94bd79d4b18f0b82
QinLei <qqinlei@gmail.com>
2021-04-21 15:54:01 +0800

    intro to stl

--- c++advance/001_stl_intro.cpp
+++ c++advance/001_stl_intro.cpp
@@ -36,10 +36,38 @@ for_each(v.begin(),v.end(),MyPrint);
 for (int i=0;i<v.size();i++){
     cout << v[i] << endl;
 }
-
 }
+
+void test02(){
+    vector< vector<int> > v;
+    vector<int> v1;
+    vector<int> v2;
+    vector<int> v3;
+    vector<int> v4;   
+
+    for (int i=0;i<4;i++){
+        v1.push_back(i);
+        v2.push_back(i+1);
+        v3.push_back(i+2);
+        v4.push_back(i+3);
+    }
+
+    v.push_back(v1);
+    v.push_back(v2);
+    v.push_back(v3);
+    v.push_back(v4);
+
+    for(vector< vector<int> >::iterator it=v.begin();it!=v.end();it++){
+        for(vector<int>::iterator vit=(*it).begin();vit!=(*it).end();vit++){
+            cout << (*vit) << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main(int argc, char const *argv[])
 {   
-    test01();
+    // test01();
+    test02();
     return 0;
 }
